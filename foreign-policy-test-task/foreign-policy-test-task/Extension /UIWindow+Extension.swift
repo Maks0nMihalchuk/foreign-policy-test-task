20962877d4601ba8e07f382163b1bd869d1fab0b
Maks0nMihalchuk <maks.mihalchuk.2000@gmail.com>
2022-08-16 22:48:36 +0300

    added helper for working with NVActivityIndicatorView and added extension for UIWindow

---
+++ foreign-policy-test-task/foreign-policy-test-task/Extension /UIWindow+Extension.swift
@@ -0,0 +1,34 @@
+//
+//  UIWindow+Extension.swift
+//  foreign-policy-test-task
+//
+//  Created by MaksOn on 16.08.2022.
+//
+
+import UIKit
+
+extension UIWindow {
+    static var key: UIWindow? {
+        if #available(iOS 13, *) {
+            return UIApplication.shared.windows.first { $0.isKeyWindow }
+        } else {
+            return UIApplication.shared.keyWindow
+        }
+    }
+
+    class func topViewController(controller: UIViewController? = UIWindow.key?.rootViewController) -> UIViewController? {
+
+        if let navigationController = controller as? UINavigationController {
+            return topViewController(controller: navigationController.visibleViewController)
+        }
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return topViewController(controller: selected)
+            }
+        }
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        return controller
+    }
+}
